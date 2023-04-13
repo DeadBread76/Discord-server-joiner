50db43bf0a9ab245efb5d2efdaf81406087ad891
SpriteyOnTheDraconic <91148727+SpriteyOnTheDraconic@users.noreply.github.com>
2023-04-13 17:38:45 +0300

    Update joiner.py
    Makes it better TBH

--- joiner.py
+++ joiner.py
@@ -1,18 +1,32 @@
 import requests
 
-link = input('Discord Invite Link: ')
-if len(link) > 6:
-    link = link[19:]
-apilink = "https://discordapp.com/api/v6/invite/" + str(link)
+# Prompt user for Discord invite link
+invite_link = input('Discord Invite Link: ')
 
-print (link)
+# Extract code from invite link
+if len(invite_link) > 6:
+    invite_code = invite_link[19:]
+else:
+    raise ValueError('Invalid invite link')
 
-with open('tokens.txt','r') as handle:
-        tokens = handle.readlines()
-        for x in tokens:
-            token = x.rstrip()
-            headers={
-                'Authorization': token
-                }
-            requests.post(apilink, headers=headers)
-        print ("All valid tokens have joined!")
+# Construct API link using invite code
+api_link = f'https://discordapp.com/api/v6/invite/{invite_code}'
+
+# Read tokens from file
+try:
+    with open('tokens.txt') as f:
+        tokens = f.read().splitlines()
+except FileNotFoundError:
+    print('Error: tokens.txt file not found')
+    exit()
+
+# Join server with valid tokens
+valid_tokens = 0
+for token in tokens:
+    headers = {'Authorization': token}
+    response = requests.post(api_link, headers=headers)
+    if response.status_code == 200:
+        valid_tokens += 1
+
+# Print number of valid tokens that joined
+print(f'{valid_tokens} valid tokens have joined the server')
